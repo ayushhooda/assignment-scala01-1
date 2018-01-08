0ab5e67c27341e2fae9495e2f190f363a4a95b15
Ayush Hooda <ayush.hooda@knoldus.in>
2018-01-08 17:50:00 +0530

    added logic for searching and sorting

---
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -0,0 +1,28 @@
+package edu.knoldus
+
+class Searching {
+
+  // Linear Search
+  def linearSearch(numbers: Array[Int], toFind: Int): Boolean = {
+    for(i <- 0 until numbers.length - 1) {
+      if(numbers(i) == toFind)
+        true
+    }
+    false
+  }
+
+  // Binary Search
+  def binarySearch(numbers: Array[Int], toFind: Int, startIndex: Int, endIndex: Int): Boolean = {
+    if(startIndex > endIndex)
+      false
+    val mid = (startIndex + (endIndex - startIndex + 1))/2
+    if(numbers(mid) == toFind)
+      true
+    else if(numbers(mid) > toFind)
+      binarySearch(numbers, toFind, startIndex, mid - 1)
+    else
+      binarySearch(numbers, toFind, mid + 1, endIndex)
+  }
+
+
+}
---
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -0,0 +1,49 @@
+package edu.knoldus
+
+class Sorting {
+
+  // Insertion Sort
+  def insertionSort(list: Array[Int]): Array[Int] = {
+    for(i <- 0 to list.length) {
+      val key = list(i)
+      var j = i - 1
+      while(j >= 0 && list(j) > key) {
+        list(j + 1) = list(j)
+        j = j - 1
+      }
+      list(j + 1) = key
+    }
+    list
+  }
+
+  // Selection Sort
+  def selectionSort(list: Array[Int]): Array[Int] = {
+    for(i <- 0 until list.length - 1) {
+      var minIndex = i
+      for(j <- i + 1 until list.length){
+        if(list(j) < list(minIndex)) {
+          minIndex = j
+          val temp = list(i)
+          list(i) = list(minIndex)
+          list(minIndex) = temp
+        }
+      }
+    }
+    list
+  }
+
+  // Bubble Sort
+  def bubbleSort(list: Array[Int]): Array[Int] = {
+    for(i <- 0 until list.length - 1) {
+      for(j <- 0 until list.length - i - 1) {
+        if(list(j) > list(j + 1)) {
+          val temp = list(j)
+          list(j) = list(j + 1)
+          list(j + 1) = temp
+        }
+      }
+    }
+    list
+  }
+
+}
